3afc398838cc0d99306933c8858cb1dd5ea0ee2d
saifur550 <srstap550@gmail.com>
2021-08-31 05:07:19 +0800

    update

---
+++ main.js
@@ -0,0 +1,26 @@
+const loader = document.getElementById("preloader");
+window.addEventListener("load", function(){
+    loader.style.display ="none"
+})
+
+const button = document.getElementById('btn').addEventListener('click', function(){
+    const searchInput = document.getElementById('searchInput').value;
+    const apiLink = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=cbadeb9ac8c0b5a2e880b17448d46c7e`
+    fetch(apiLink)
+    .then( res => res.json())
+    .then( data =>showInfo(data))
+
+})
+
+const img = document.getElementById('img')
+const country = document.getElementById('country')
+const temp = document.getElementById('temp')
+const status = document.getElementById('status')
+
+//showinfo function
+function showInfo(info){
+    console.log(info);
+    country.innerText = info.name;
+    temp.innerText = info.weather[0].main;
+    status.innerText = `${(info.main.temp - 273.15).toFixed(2)}`;
+} 
